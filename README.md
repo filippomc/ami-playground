2b5842b9f7ec4468ab821b86665d3cdeab820459
Filippo Ledda <filippo@metacell.us>
2023-02-14 15:08:58 +0100

    Working two layered example

---
+++ README.md
@@ -0,0 +1,16 @@
+# AMI.js playground
+
+Playground to create AMI.js usage examples
+
+Install libraries
+```
+yarn install
+```
+
+Run locally
+
+```
+npx parcel index.html
+```
+
+To change the example, modify index.html to load the proper file
---
+++ utils.js
@@ -0,0 +1,6 @@
+export const file = 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/adi_brain/adi_brain.nii.gz';
+export const file2 = 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/eun_brain/eun_uchar_8.nii.gz';
+export const colors = {
+  red: 0xff0000,
+  darkGrey: 0x353535,
+};
