10a6b6f0bd54ac6c49af844e46515533f87ece05
afonso <afonsobspinto@gmail.com>
2023-02-27 17:20:40 +0000

    fix: Hotfix relate translateX views

--- components/dicom-viewer/SagitalHelper.js
+++ components/dicom-viewer/SagitalHelper.js
@@ -8,7 +8,7 @@ export class SagittalHelper extends ViewHelper{
 
     translateX(valuePercentage) {
         // Translating the object on the X axis makes the overlay move along the X axis from the sagittal perspective
-        const sideLength = this.sideLength
+        const sideLength = this.sideLength / 2 // fixme: distance of the camera to the object seems to be double here I'm not sure why thus the / 2
         const pixelsToMove = valuePercentage * sideLength / 100
         super.translate(X, pixelsToMove)
     }
